9ef86123b10bfde99ac920bb3e0b36ea74cb0d69
Romi <romi.mateos2005@gmail.com>
2026-09-18 22:23:30 +0200

    feat: add base Order class with partial fill logic

--- order_book.py
+++ order_book.py
@@ -1,8 +1,20 @@
 import numpy as np
+import uuid
 
 class Order:
     def __init__(self, side: str, price: float, amount: float):
             self.side = side
             self.price = price
             self.amount = amount
-            self.id = np.random.random_integers(0, 1000000)
+            self.id = uuid.uuid4().hex[:8]
+            self.active = True
+
+    def __repr__(self):
+        return f'Order: side = {self.side}, price = {self.price}, amount = {self.amount}, active = {self.active}'
+        
+    def reduce_amount(self, executed_amount):
+        if self.amount > executed_amount:
+            self.amount -= executed_amount
+        else:
+             self.amount = 0
+             self.active = False
