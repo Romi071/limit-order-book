0677e6f2512823acbbe65d38c11380b1477614f6
Romi <romi.mateos2005@gmail.com>
2026-09-22 18:22:46 +0200

    feat: redesign OrderBook class to handle buy-side partial fills, full price-level sweeps and heap synchronization for incoming buy orders

--- order_book.py
+++ order_book.py
@@ -39,6 +39,7 @@ class OrderQueue:
     def evict_order(self):
         if len(self.queue) > 0:
             self.volume -= (self.queue[0]).amount
+            self.queue[0].active = False
             self.queue.popleft()
         else:
             print("The operation couldn't be completed, there are no orders left to evict")
@@ -54,27 +55,62 @@ class OrderBook:
     def __repr__(self):
         return f'OrderBook Object:\nCurrent bids = {self.bids}\nCurrent asks = {self.asks}'
 
+    #Method to fill orders or keep them in the book (ordered using heapq O(log(N))) if current market does not allow for instant filling
     def add_order(self, order):
+        #Buy orders:
         if order.side == "buy":
-            if len(self.sells_heap) > 0 and order.price >= self.sells_heap[0]: 
+            #Loop over the sells_heap best prices
+            while order.active == True and len(self.sells_heap) > 0:
                 best_sell_price = self.sells_heap[0]
-                self.asks[best_sell_price].evict_order()
-                if self.asks[best_sell_price].volume == 0:
-                    heapq.heappop(self.sells_heap)
-            else:
+                best_orderq = self.asks[best_sell_price]
+                #If the best available sell price is under the buy order's offer
+                if order.price >= best_sell_price:
+                    #If the order's amount is greater than the total volume in the OrderQueue at this price
+                    if order.amount >= best_orderq.volume:
+                        order.reduce_amount(best_orderq.volume)
+                        #Fully fill the OrderQueue at this price
+                        while len(best_orderq.queue) > 0:
+                            best_orderq.evict_order()
+                    #Volume in the OrderQueue at this price greater than the order's amount
+                    else:
+                        #Loop over the OrderQueue orders until the order is filled
+                        while order.active == True:
+                            e = best_orderq.queue[0]
+                            trade_amount = min(order.amount, e.amount)
+                            order.reduce_amount(trade_amount)
+                            if trade_amount == e.amount:
+                                best_orderq.evict_order()
+                            else:
+                                e.reduce_amount(trade_amount)
+                                best_orderq.volume -= trade_amount
+                    #If the OrderQueue entry at this price is fully filled, pop price from the heapq
+                    if best_orderq.volume == 0:
+                        heapq.heappop(self.sells_heap)
+                #Best available sell price is above the buy order's offer
+                else:
+                    break
+            #If the buy order still remains to be filled, place it waiting in the book
+            if order.active == True:
                 if order.price not in self.bids:
                     self.bids[order.price] = OrderQueue(order.price)
                     self.bids[order.price].ingest_order(order)
+                    #Store the price as negative to use the Min-Heap module as a Max-Heap for the buys heapq
                     heapq.heappush(self.buys_heap, -order.price)
                 else:
                     self.bids[order.price].ingest_order(order)
 
+
+        ## UNFINISHED PART:
+        #Sell Orders:
         elif order.side == "sell":
+            #If possible, fill the sell order instantly
             if len(self.buys_heap) > 0 and order.price <= -self.buys_heap[0]: 
                 best_buy_price = -self.buys_heap[0]
                 self.bids[best_buy_price].evict_order()
+                #If the OrderQueue entry is fully filled, pop from the heapq
                 if self.bids[best_buy_price].volume == 0:
                     heapq.heappop(self.buys_heap)
+            #Place the sell order waiting in the book
             else:
                 if order.price not in self.asks:
                     self.asks[order.price] = OrderQueue(order.price)
